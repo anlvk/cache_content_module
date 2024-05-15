7c6d85395316dc21f73b08eabee77e37e405d56d
Hanna Liauchuk <hanna.liauchuk@innowise.com>
2024-05-15 15:30:57 +0200

    Add end lines.

--- src/Plugin/Block/CacheContentSecondBlock.php
+++ src/Plugin/Block/CacheContentSecondBlock.php
@@ -20,28 +20,28 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class CacheContentSecondBlock extends BlockBase implements ContainerFactoryPluginInterface {
-    /**
-     * Defines the interface for a configuration object factory.
-     * 
-     * @var $configFactory \Drupal\Core\Config\ConfigFactoryInterface
-     */
-    protected $configFactory;
+  /**
+   * Defines the interface for a configuration object factory.
+   * 
+   * @var $configFactory \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
 
-    /**
-     * Instantiates a new instance of this class.
-     *
-     * @param ContainerInterface $container
-     * @param array $configuration
-     * @param [type] $plugin_id
-     * @param [type] $plugin_definition
-     * @return object
-     */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): object {
-        $instance = new static($configuration, $plugin_id, $plugin_definition);
-        $instance->configFactory = $container->get('config.factory');
+  /**
+   * Instantiates a new instance of this class.
+   *
+   * @param ContainerInterface $container
+   * @param array $configuration
+   * @param [type] $plugin_id
+   * @param [type] $plugin_definition
+   * @return object
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): object {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->configFactory = $container->get('config.factory');
 
-        return $instance;
-    }
+    return $instance;
+  }
 
   /**
    * Builds the content block.
